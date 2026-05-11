074511a186f4f2b2e30c7515ef604a9578b74046
rhsonoga <rhs.onoga@gmail.com>
2026-05-11 15:05:25 +0700

    Disable premium overlay - allow all users access during feature testing

--- webapp/premium/access.py
+++ webapp/premium/access.py
@@ -9,11 +9,18 @@ def get_auth_status(auth_user=None):
         'verified': User is logged in and email is verified
         'need_verify': User is logged in but email is not verified
         'not_login': User is not logged in
+        
+    NOTE: Currently disabled for feature testing - always returns 'verified'
     """
-    if not auth_user:
-        return 'not_login'
-
-    email_verified_at = auth_user.get('email_verified_at')
-    if email_verified_at:
-        return 'verified'
-    return 'need_verify'
+    # [FEATURE TESTING] Temporarily allow all users to access premium features
+    # Uncomment below to re-enable premium access control
+    return 'verified'
+    
+    # Original logic (disabled):
+    # if not auth_user:
+    #     return 'not_login'
+    #
+    # email_verified_at = auth_user.get('email_verified_at')
+    # if email_verified_at:
+    #     return 'verified'
+    # return 'need_verify'
